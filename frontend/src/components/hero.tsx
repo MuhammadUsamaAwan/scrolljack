75e3edfd06dd1a5eed1d376d92c6b833eebb7d9a
MuhammadUsamaAwan <underusama@gmail.com>
2025-07-19 12:31:58 +0500

    chore: frontend setup

---
+++ frontend/src/components/hero.tsx
@@ -0,0 +1,10 @@
+export function Hero() {
+  return (
+    <div className='text-center'>
+      <h1 className='font-bold text-5xl'>📜Scrolljack</h1>
+      <p className='mt-4 text-lg text-muted-foreground'>
+        Turn your <code>.wabbajack</code> files into readable modding guides
+      </p>
+    </div>
+  );
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [tsConfigPaths(), tailwindcss(), react()],
+});
